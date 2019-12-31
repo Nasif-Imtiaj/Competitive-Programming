ebacde37bc587889fb9f860e2e7b8bd0349fac53
Nasif-Imtiaj <nasifimtiaj.16@gmail.com>
2019-12-31 17:09:35 +0600

    changed header file

--- Data Structure/Binary Index tree.cpp
+++ Data Structure/Binary Index tree.cpp
@@ -1,5 +1,14 @@
 #include<bits/stdc++.h>
 using namespace std;
+///Welcome to Nasif's Code
+#define co(q) cout<<q<<endl;
+typedef long long int ll;
+typedef unsigned long long int ull;
+const int MOD = (int)1e9+7;
+const int MAX = 1e6;
+#define pi acos(-1)
+#define bug cout<<"bug"<<endl;
+#define FastRead     ios_base::sync_with_stdio(false);cin.tie(NULL);
 int tree[100001];
 int arr[100000];
 int query(int idx)
